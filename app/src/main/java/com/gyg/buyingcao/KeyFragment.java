3811bdb63865c31290668d73fb6605362a6955e5
yongqguo@yeah.net <yongqguo@yeah.net>
2019-05-28 00:13:55 +0800

    a222

--- app/src/main/java/com/gyg/buyingcao/KeyFragment.java
+++ app/src/main/java/com/gyg/buyingcao/KeyFragment.java
@@ -47,16 +47,28 @@ public class KeyFragment extends Fragment {
             public void onClick(View view) {
                 String patentPath = Environment.getExternalStorageDirectory().getPath()+"/download/";
                 try {
-                    Intent intent = new Intent(Intent.ACTION_EDIT);
                     File file = new File(patentPath,"CN"+strCaseNum.substring(0,strCaseNum.length()-1) + ".2.txt");
                     if(!file.exists()){
                         Toast.makeText(getActivity(),"找不到文件:\n" + "CN"+strCaseNum.substring(0,strCaseNum.length()-1) + ".2.txt", Toast.LENGTH_LONG).show();
                         return;
                     }
+                  //  Uri uri = FileProvider7.getUriForFile(getContext(),file);
+                    Intent intent = new Intent();
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    //设置intent的Action属性
+                    intent.setAction(Intent.ACTION_VIEW);
+                    //获取文件file的MIME类型
+                    String type = "text/plain";
+                    //设置intent的data和Type属性。
                     Uri uri = FileProvider7.getUriForFile(getContext(),file);
-                    intent.putExtra(Intent.EXTRA_TEXT, uri);  //传输图片或者文件 采用流的方式
-                    intent.setType("*/*");   //分享文件
-                    startActivity(Intent.createChooser(intent, "编辑方式选择"));
+                    intent.setDataAndType(uri, type);
+                    startActivity(intent);
+//                    Intent intent = new Intent();
+//                    intent.setAction(Intent.ACTION_VIEW);
+//                    String type = "text/*";
+//                    FileProvider7.setIntentDataAndType(getContext(),intent,type,file,true);
+//                //    intent.setDataAndType(uri,type);
+//                    startActivity(intent);
                 }catch (Exception e) {
                     Toast.makeText(getActivity(),"Error on action send:\n" + e, Toast.LENGTH_LONG).show();
                 }
@@ -68,26 +80,26 @@ public class KeyFragment extends Fragment {
                 String patentPath = Environment.getExternalStorageDirectory().getPath()+"/download/";
                 if(!writeTxtToFile(strCaseNum,patentPath,"k"+strCaseNum + ".2.txt"))
                     return;
-                Toast.makeText(getActivity(),"写文件成功：\nt" + strCaseNum + ".2.txt", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(),"写文件成功：\nk" + strCaseNum + ".2.txt", Toast.LENGTH_SHORT).show();
                 try {
-                    Intent intent = new Intent();
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    //设置intent的Action属性
-                    intent.setAction(Intent.ACTION_VIEW);
-                    //获取文件file的MIME类型
-                    String type = "text/plain";
-                    //设置intent的data和Type属性。
-                    File file = new File(patentPath,"k"+strCaseNum + ".2.txt");
-                    Uri uri = FileProvider7.getUriForFile(getContext(),file);
-                    intent.setDataAndType(uri, type);
-                    startActivity(intent);
-
-//                    Intent intent = new Intent(Intent.ACTION_SEND);
+//                    Intent intent = new Intent();
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    //设置intent的Action属性
+//                    intent.setAction(Intent.ACTION_VIEW);
+//                    //获取文件file的MIME类型
+//                    String type = "text/plain";
+//                    //设置intent的data和Type属性。
 //                    File file = new File(patentPath,"k"+strCaseNum + ".2.txt");
 //                    Uri uri = FileProvider7.getUriForFile(getContext(),file);
-//                    intent.putExtra(Intent.EXTRA_STREAM, uri);  //传输图片或者文件 采用流的方式
-//                    intent.setType("text/plain");   //分享文件
-//                    startActivity(Intent.createChooser(intent, "分享"));
+//                    intent.setDataAndType(uri, type);
+//                    startActivity(intent);
+
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    File file = new File(patentPath,"k"+strCaseNum + ".2.txt");
+                    Uri uri = FileProvider7.getUriForFile(getContext(),file);
+                    intent.putExtra(Intent.EXTRA_STREAM, uri);  //传输图片或者文件 采用流的方式
+                    intent.setType("*/*");   //分享文件
+                    startActivity(Intent.createChooser(intent, "分享"));
                 }catch (Exception e) {
                     Toast.makeText(getActivity(),"Error on action send:\n" + e, Toast.LENGTH_LONG).show();
                 }
